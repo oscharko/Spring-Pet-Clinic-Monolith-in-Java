d24ed54780413ff2d63a1448a37acf4738f4d0ef
oscharko <o.scharkowski@oscharko.de>
2022-07-25 11:24:46 +0200

    Delete exception...

--- Spring-Pet-Clinic-Monolith-in-Jave/Pet-Clinic-Web/src/main/java/de/oscharko/springpetclinicmonolith/bootstrap/DataLoader.java
+++ Spring-Pet-Clinic-Monolith-in-Jave/Pet-Clinic-Web/src/main/java/de/oscharko/springpetclinicmonolith/bootstrap/DataLoader.java
@@ -29,7 +29,7 @@ public class DataLoader implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
