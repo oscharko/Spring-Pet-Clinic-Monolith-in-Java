70c5d8283ce79098f182263cd4b1ccef291c8564
oscharko <o.scharkowski@oscharko.de>
2022-07-27 21:09:11 +0200

    Add Contact Info Properties to Owner closes #28

--- Spring-Pet-Clinic-Monolith-in-Java/Pet-Clinic-Data/src/main/java/de/oscharko/springpetclinicmonolith/model/Owner.java
+++ Spring-Pet-Clinic-Monolith-in-Java/Pet-Clinic-Data/src/main/java/de/oscharko/springpetclinicmonolith/model/Owner.java
@@ -11,15 +11,40 @@ import java.util.Set;
  * Inside the package - de.oscharko.springpetclinicmonolith.model
  */
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
+    private Set<Pet> pets;
 
-	private Set<Pet> pets;
+    public String getAddress() {
+        return address;
+    }
 
-	public Set<Pet> getPets() {
-		return pets;
-	}
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-	public void setPets(Set<Pet> pets) {
-		this.pets = pets;
-	}
+    public String getCity() {
+        return city;
+    }
 
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
