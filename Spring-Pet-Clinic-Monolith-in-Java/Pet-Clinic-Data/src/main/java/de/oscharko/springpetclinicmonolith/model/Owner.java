e7277cf89995f5efba84b1e4c37cdd7f2c5c8206
oscharko <o.scharkowski@oscharko.de>
2022-07-27 11:18:00 +0200

    Spelling in project-name jave to java

--- Spring-Pet-Clinic-Monolith-in-Java/Pet-Clinic-Data/src/main/java/de/oscharko/springpetclinicmonolith/model/Owner.java
+++ Spring-Pet-Clinic-Monolith-in-Java/Pet-Clinic-Data/src/main/java/de/oscharko/springpetclinicmonolith/model/Owner.java
@@ -1,10 +1,20 @@
 package de.oscharko.springpetclinicmonolith.model;
 
+import java.util.Set;
+
 /**
  * Created by oscharko on 13.07.22 😎 Check out -> www.oscharko.de --------------------
  * Inside the module - Pet-Clinic-Data Inside the package -
  * de.oscharko.springpetclinicmonolith.model --------------------
  */
 public class Owner extends Person {
+    private Set<Pet> pets;
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
