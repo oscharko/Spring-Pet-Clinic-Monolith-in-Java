45b075145a3209712956487d8817e7707e72c94e
oscharko <o.scharkowski@oscharko.de>
2022-07-27 11:28:00 +0200

    Create Pet Type, Pet, and Visit Entities closes #26

--- Spring-Pet-Clinic-Monolith-in-Java/Pet-Clinic-Data/src/main/java/de/oscharko/springpetclinicmonolith/model/Visit.java
+++ Spring-Pet-Clinic-Monolith-in-Java/Pet-Clinic-Data/src/main/java/de/oscharko/springpetclinicmonolith/model/Visit.java
@@ -2,6 +2,17 @@ package de.oscharko.springpetclinicmonolith.model;
 
 import java.time.LocalDate;
 
+/**
+ * IntelliJ IDEA 2022.2 (Ultimate Edition)
+ * --------------------
+ * Created by oscharko on 27.07.22 😎
+ * Check out -> www.oscharko.de
+ * --------------------
+ * Spring-Pet-Clinic-Monolith-in-Java
+ * Inside the module - Pet-Clinic-Data
+ * Inside the package - de.oscharko.springpetclinicmonolith.model
+ * --------------------
+ */
 public class Visit extends BaseEntity {
     private LocalDate date;
     private String description;
