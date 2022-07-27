24c9ac9526e1bd16db3cdef259821b5f2fd086bf
oscharko <o.scharkowski@oscharko.de>
2022-07-27 21:33:11 +0200

    Create Specialty Map Service Implement closes #31

---
+++ Spring-Pet-Clinic-Monolith-in-Java/Pet-Clinic-Data/src/main/java/de/oscharko/springpetclinicmonolith/services/SpecialtiesService.java
@@ -0,0 +1,15 @@
+package de.oscharko.springpetclinicmonolith.services;
+
+import de.oscharko.springpetclinicmonolith.model.Speciality;
+
+/**
+ * IntelliJ IDEA 2022.2 (Ultimate Edition)
+ * Created by oscharko on 27.07.22 😎
+ * Check out -> www.oscharko.de
+ * Spring-Pet-Clinic-Monolith-in-Java
+ * Inside the module - Pet-Clinic-Data
+ * Inside the package - de.oscharko.springpetclinicmonolith.services
+ */
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
---
+++ Spring-Pet-Clinic-Monolith-in-Java/Pet-Clinic-Data/src/main/java/de/oscharko/springpetclinicmonolith/services/map/SpecialityMapService.java
@@ -0,0 +1,41 @@
+package de.oscharko.springpetclinicmonolith.services.map;
+
+import de.oscharko.springpetclinicmonolith.model.Speciality;
+import de.oscharko.springpetclinicmonolith.services.SpecialtiesService;
+
+import java.util.Set;
+
+/**
+ * IntelliJ IDEA 2022.2 (Ultimate Edition)
+ * Created by oscharko on 27.07.22 😎
+ * Check out -> www.oscharko.de
+ * Spring-Pet-Clinic-Monolith-in-Java
+ * Inside the module - Pet-Clinic-Data
+ * Inside the package - de.oscharko.springpetclinicmonolith.services.map
+ */
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
