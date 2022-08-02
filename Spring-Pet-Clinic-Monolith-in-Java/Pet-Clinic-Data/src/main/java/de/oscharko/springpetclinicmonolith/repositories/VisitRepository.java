82193884d8589fb73876d5474e068808bd10e588
oscharko <o.scharkowski@oscharko.de>
2022-08-02 22:25:09 +0200

    Implements data JPA repositories closes #39

---
+++ Spring-Pet-Clinic-Monolith-in-Java/Pet-Clinic-Data/src/main/java/de/oscharko/springpetclinicmonolith/repositories/VisitRepository.java
@@ -0,0 +1,15 @@
+package de.oscharko.springpetclinicmonolith.repositories;
+
+import de.oscharko.springpetclinicmonolith.model.Visit;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * IntelliJ IDEA 2022.2 (Ultimate Edition)
+ * Created by oscharko on 02.08.22 😎
+ * Check out -> www.oscharko.de
+ * Spring-Pet-Clinic-Monolith-in-Java
+ * Inside the module - Pet-Clinic-Data
+ * Inside the package - de.oscharko.springpetclinicmonolith.repositories
+ */
+public interface VisitRepository extends CrudRepository<Visit, Long> {
+}
